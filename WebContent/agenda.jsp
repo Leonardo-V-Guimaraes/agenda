59fa2cf23718391fe4b1d98cbb94fbbb697ae8fd
Leonardo-V-Guimaraes <leoguimavg@hotmail.com>
2021-07-21 19:09:36 -0300

    Adicionada tabela de contatos

--- WebContent/agenda.jsp
+++ WebContent/agenda.jsp
@@ -1,5 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
-    pageEncoding="utf-8"%>
+	pageEncoding="utf-8"%>
+<%@ page import="model.JavaBeans"%>
+<%@ page import="java.util.ArrayList"%>
+<%
+     ArrayList<JavaBeans> lista = (ArrayList<JavaBeans>) request.getAttribute("contatos");
+/**for (int i = 0; i < lista.size(); i++) {
+	out.println(lista.get(i).getIdcon());
+}*/
+%>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,7 +17,27 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Agenda de Contatos</h1>
-    <a href="novo.html" class="Botao1">Novo Contato</a>
+	<h1>Agenda de Contatos</h1>
+	<a href="novo.html" class="Botao1">Novo Contato</a>
+	<table>
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Nome</th>
+				<th>Fone</th>
+				<th>E-mail</th>
+			</tr>
+		</thead>
+		<tbody>
+			<%for (int i = 0; i < lista.size(); i++) { %>
+			<tr>
+				<td><%=lista.get(i).getIdcon()%></td>
+				<td><%=lista.get(i).getNome()%></td>
+				<td><%=lista.get(i).getFone()%></td>
+				<td><%=lista.get(i).getEmail()%></td>
+			</tr>
+			<% } %>
+		</tbody>
+	</table>
 </body>
 </html>
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,17 +1,23 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.DAO;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+import model.DAO;
+import model.JavaBeans;
+
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -23,13 +29,49 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 
 	// Listar contatos
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.sendRedirect("agenda.jsp");
+		// Criando um objeto que irá receber os dados JavaBeans
+		ArrayList<JavaBeans> lista = dao.listarContatos();
+		// Encaminhar a lista ao documento agenda.jsp
+		request.setAttribute("contatos", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+		rd.forward(request, response);
+		
+		/* Teste de recebimento da lista
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i).getIdcon());
+			System.out.println(lista.get(i).getNome());
+			System.out.println(lista.get(i).getFone());
+			System.out.println(lista.get(i).getEmail());
+		}*/
+	}
+
+	// Novo contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/* test de recebimento dos dados do formulario
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("fone"));
+		System.out.println(request.getParameter("email"));
+		*/
+		// setar as variaveis JavaBeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		// invocar o metodo inserirContato passando o objeto contato
+		dao.inserirContato(contato);
+		// redirecionar para o documento agenda.jsp
+		response.sendRedirect("main");
+		
 	}
 
 }
